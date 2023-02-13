96b24b6288a9507f1a37bf7987f8d0efb4f35292
conrtv <38775548+conrtv@users.noreply.github.com>
2023-02-13 15:46:42 -0600

    reset code

--- src/App.js
+++ src/App.js
@@ -2,19 +2,8 @@ import './App.css';
 
 
 function App() {
-  const planets = [
-    { name: "Mars", isGasPlanet : false },
-    { name: "Earth", isGasPlanet : false },
-    { name: "Jupiter", isGasPlanet : true },
-    { name: "Venus", isGasPlanet : false },
-    { name: "Neptune", isGasPlanet : true },
-  ];
-
-
   return (
   <div className="App">
-    {planets.map((planet, key) => planet.isGasPlanet && <h1> {planet.name} </h1>
-    )}
   </div>
   );
 }
