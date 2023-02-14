82ed27d304b5b2c99efab0a2ba92265e7b86166f
conrtv <38775548+conrtv@users.noreply.github.com>
2023-02-14 12:00:50 -0600

    branch for crud app

--- src/App.js
+++ src/App.js
@@ -4,17 +4,8 @@ import { useState } from "react";
 
 function App() {
 
-const [count, setCount] = useState(0);
-
   return (
   <div className="App">
-
-  <button onClick={() => {setCount(count + 1)}}>Increase</button>
-  <button onClick={() => {setCount(count - 1)}}>Decrease</button>
-  <button onClick={() => {setCount(0)}}>Set to Zero</button>
-
-  {count}
-
   </div>
 
 );
